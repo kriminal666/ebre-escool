32013df175e11ec151f6ab319a2bc394cb913581
Sergi Tur Badenas <sergitur@ebretic.com>
2013-10-08 12:18:22 +0200

    ok

--- view_webfaltes.sql
+++ view_webfaltes.sql
@@ -6,6 +6,6 @@ groupName,
 groupDescription,
 educationalLevelId,
 grade,
-mentorid
+mentorId
 ) AS SELECT group_id, codi_grup, nom_grup, nom_grup AS groupName, descripcio, nivell_educatiu, grau AS grade, tutor
 FROM webfaltes.grup
