ec723303ec75907101d283219dbb795956dbf05f
Sergi Tur Badenas <sergitur@ebretic.com>
2013-10-08 12:17:02 +0200

    View

---
+++ view_webfaltes.sql
@@ -0,0 +1,11 @@
+CREATE ALGORITHM = UNDEFINED VIEW classroom_group_view(
+groupId,
+groupCode,
+groupShortName,
+groupName,
+groupDescription,
+educationalLevelId,
+grade,
+mentorid
+) AS SELECT group_id, codi_grup, nom_grup, nom_grup AS groupName, descripcio, nivell_educatiu, grau AS grade, tutor
+FROM webfaltes.grup
