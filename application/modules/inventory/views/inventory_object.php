f0d9c509448b7caba8a65b6758315f3638cb8043
Sergi Tur Badenas <sergitur@ebretic.com>
2014-02-22 13:51:12 +0100

    sync

--- application/modules/inventory/views/inventory_object.php
+++ application/modules/inventory/views/inventory_object.php
@@ -1,5 +1,5 @@
 <div class="main-content">
-
+ 
 <div id="breadcrumbs" class="breadcrumbs">
  <script type="text/javascript">
   try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
@@ -16,7 +16,9 @@
  </ul>
 </div>
 
-        <div class="page-header position-relative">
+<div class="page-content">
+
+<div class="page-header position-relative">
                         <h1>
                             <a href="<?php echo base_url('index.php/inventory/inventory');?>"><?php echo lang("inventory");?></a>
                             <small>
@@ -24,33 +26,271 @@
                                 Objectes
                             </small>
                         </h1>
-        </div><!-- /.page-header -->
+</div>
 
-	<div style="margin:10px;">
-   		
-      <!-- Load Grocery Crud -->
+<div class="space-3"></div>
+
+              <div class="row-fluid">
+                
+                <div class="span1"></div>
+
+                <div class="span5 widget-container-span">
+                  <div class="widget-box collapsed">
+                    <div class="widget-header widget-header-small header-color-green">
+                      <h6>
+                        <i class="icon-sort"></i>
+                        Manteniments
+                      </h6>
+
+                      <div class="widget-toolbar">
+                        <a href="#" data-action="collapse">
+                          <i class="icon-chevron-down"></i>
+                        </a>
+
+                        <a href="#" data-action="close">
+                          <i class="icon-remove"></i>
+                        </a>
+                      </div>
+                    </div>
+
+                    <div class="widget-body">
+                      <div class="widget-main">                        
+
+                    <ol class="dd-list">
+                      <li class="dd-item dd2-item" data-id="13">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt blue bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">Tipus Identificadors externs</a></div>
+                      </li>
+
+                      <li class="dd-item dd2-item" data-id="15">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt orange bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">Tipus Material</a></div>
+
+                      </li>
+
+                      <li class="dd-item dd2-item" data-id="19">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt blue bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">Marques</a></div>
+                      </li>
+
+                      <li class="dd-item dd2-item" data-id="19">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt blue bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">Models</a></div>
+                      </li>
+
+                      <li class="dd-item dd2-item" data-id="19">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt blue bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">Proveïdors</a></div>
+                      </li>
+
+                      <li class="dd-item dd2-item" data-id="19">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt blue bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">Origen dels diners</a></div>
+                      </li>
+
+                      <li class="dd-item dd2-item" data-id="19">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt blue bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">Codis de barres</a></div>
+                      </li>
+                    </ol>
+
+
+
+
+
+
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                      
+                <div class="span5 widget-container-span">
+                  <div class="widget-box collapsed">
+                    <div class="widget-header widget-header-small header-color-orange">
+                      <h6>
+                        <i class="icon-sort"></i>
+                        Informes d'inventari
+                      </h6>
+
+                      <div class="widget-toolbar">
+                        <a href="#" data-action="collapse">
+                          <i class="icon-chevron-down"></i>
+                        </a>
+
+                        <a href="#" data-action="close">
+                          <i class="icon-remove"></i>
+                        </a>
+                      </div>
+                    </div>
+
+                    <div class="widget-body">
+                      <div class="widget-main">                        
+
+                    <ol class="dd-list">
+                      <li class="dd-item dd2-item" data-id="13">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt blue bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">Informe 1 TODO</a></div>
+                      </li>
+
+                      <li class="dd-item dd2-item" data-id="15">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt orange bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">Informe 2 TODO</a></div>
+
+                      </li>
+
+                      <li class="dd-item dd2-item" data-id="19">
+                        <div class="dd-handle dd2-handle">
+                          <i class="normal-icon icon-download-alt blue bigger-130"></i>
+
+                          <i class="drag-icon icon-move bigger-125"></i>
+                        </div>
+                        <div class="dd2-content"><a href="#">INFORME 3 TODO</a></div>
+                      </li>
+                    </ol>
+
+
+
+
+
+
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                
+
+              <div class="span2"></div>
+            </div>
+
+
+      <div class="table-header">
+    <i class="icon-group"></i> 
+    <div class="inline position-relative">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown">
+                            <?php echo "selected_department_name";?>
+                            <i class="icon-angle-down icon-on-right bigger-110"></i>
+                          </button>
+
+                          <ul class="dropdown-menu pull-right dropdown-125 dropdown-lighter dropdown-caret">
+                          <?php foreach ($departments as $department_key => $department): ?>
+                            <li <?php if ($selected_department_key == $department_key ) { echo 'class="active"';} ?> >
+                              <a href="#" 
+                                <?php if ($selected_department_key == $department_key ) { echo 'class="blue"';}?> >
+                                <i class="icon-caret-right bigger-110">&nbsp;</i>
+                                <?php echo $department; ?>
+                              </a>
+                            </li>      
+                          <?php endforeach; ?>
+                          </ul>
+    </div>
+    <i class="icon-double-angle-right"></i> 
+    <div class="inline position-relative">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown">
+                            <?php echo "selected_classroom_group_shortname";?>
+                            <i class="icon-angle-down icon-on-right bigger-110"></i>
+                          </button>
+
+                          <ul class="dropdown-menu pull-right dropdown-125 dropdown-lighter dropdown-caret">
+                          <?php foreach ($classroom_groups as $classroom_group_key => $classroom_group): ?>
+                            <li <?php if ($selected_classroom_group_key == $classroom_group_key ) { echo 'class="active"';} ?> >
+                              <a href="#" 
+                                <?php if ($selected_classroom_group_key == $classroom_group_key ) { echo 'class="blue"';}?> >
+                                <i class="icon-caret-right bigger-110">&nbsp;</i>
+                                <?php echo $classroom_group; ?>
+                              </a>
+                            </li>      
+                          <?php endforeach; ?>
+                          </ul>
+    </div>
+    <i class="icon-double-angle-right"></i> 
+    <div class="inline position-relative">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown">
+                            <?php echo "selected_study_module_shortname";?>
+                            <i class="icon-angle-down icon-on-right bigger-110"></i>
+                          </button>
+
+                          <ul class="dropdown-menu pull-right dropdown-125 dropdown-lighter dropdown-caret">
+                          <?php foreach ($study_modules as $study_module_key => $study_module): ?>
+                            <li <?php if ($selected_study_module_key == $study_module_key ) { echo 'class="active"';} ?> >
+                              <a href="#" 
+                                <?php if ($selected_study_module_key == $study_module_key ) { echo 'class="blue"';}?> >
+                                <i class="icon-caret-right bigger-110">&nbsp;</i>
+                                <?php echo $study_module; ?>
+                              </a>
+                            </li>      
+                          <?php endforeach; ?>
+                          </ul>
+    </div>
+
+    <i class="icon-user" style="margin-left:50px;"></i> # Objectes: <?php echo " " . "total_number_of_students";?> 
+
+    <div class="inline position-relative" style="float:right;">
+              Professors del grup: 
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown">
+                            <?php echo "selected_group_teacher";?>
+                            <i class="icon-angle-down icon-on-right bigger-110"></i>
+                          </button>
+
+                          <ul class="dropdown-menu pull-right dropdown-125 dropdown-lighter dropdown-caret">
+                          <?php foreach ($group_teachers as $group_teacher_key => $group_teacher): ?>
+                            <li <?php if ($group_teachers_default_teacher_key == $group_teacher_key ) { echo 'class="active"';} ?> >
+                              <a href="#" 
+                                <?php if ($group_teachers_default_teacher_key == $group_teacher_key ) { echo 'class="blue"';}?> >
+                                <i class="icon-caret-right bigger-110">&nbsp;</i>
+                                <?php echo $group_teacher; ?>
+                              </a>
+                            </li>      
+                          <?php endforeach; ?>
+                          </ul>
+    </div>
+  </div>
+
+        <!-- Load Grocery Crud -->
       <?php echo $output; ?>
 
 
 
-
-	</div>	
 </div>
-
-<script type="text/javascript">
-
-$(document).ready(function() {
+      
 
 
-//console.debug("provaheyfar");
-
-/*new FixedColumns( oTableArray[0], {
-    "iLeftColumns": 1,
-    "iRightColumns": 1
-  } );
-*/
-
-
-});
-
-</script>
+</div>
