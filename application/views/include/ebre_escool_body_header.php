085089822ce8653719c2775b90573b2fdb007051
Oscar Adan <oadan79@gmail.com>
2014-03-13 10:37:43 +0100

    Menú lateral utilitzant búcle en lloc de switch

--- application/views/include/ebre_escool_body_header.php
+++ application/views/include/ebre_escool_body_header.php
@@ -799,6 +799,26 @@
       $(".open").removeClass("open");      
       $(".active").removeClass("active");   
 
+      if(menu_count>1){
+
+        for(var i=0; i<=menu_count; i++)
+        {
+          if(i==0){
+            $(menu['menu']).addClass("open active");
+          } else if(i==menu_count) {
+            $(menu['submenu'+i]).addClass("active");
+          } else {
+            $(menu['submenu'+i]).addClass("open active");
+          }
+        }
+
+      } else {
+        $(menu['menu']).addClass("active");
+      }
+
+
+
+/*
       switch(menu_count)
       {
         case 1:
@@ -824,7 +844,7 @@
         break;        
 
       }
-   
+  */ 
 
   });
 
