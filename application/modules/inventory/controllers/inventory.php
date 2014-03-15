d2210698fa7f5f029eea0d808ff36c753d63f580
Sergi Tur Badenas <sergitur@ebretic.com>
2014-03-15 18:48:31 +0100

    Solved  Cannot redeclare inventory::$html_header_view in /usr/share/ebre-escool/application/modules/inventory/controllers/inventory.php

--- application/modules/inventory/controllers/inventory.php
+++ application/modules/inventory/controllers/inventory.php
@@ -13,9 +13,6 @@ class inventory extends skeleton_main {
 
 	public $body_header_lang_file ='ebre_escool_body_header' ;
 
-    public $html_header_view ='include/ebre_escool_html_header' ;
-
-    public $body_footer_view ='include/ebre_escool_body_footer' ;   
 
 function __construct()	{
 
