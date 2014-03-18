4ba063130f72293d58c8539eb3df5e0abe5c1157
Sergi Tur Badenas <sergitur@ebretic.com>
2014-03-18 10:14:47 +0100

    Some forgotten files

---
+++ application/views/include/groups_view.php
@@ -0,0 +1,31 @@
+<div class="main-content">
+ 
+<div id="breadcrumbs" class="breadcrumbs">
+ <script type="text/javascript">
+  try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
+ </script>
+ <ul class="breadcrumb">
+  <li>
+   <i class="icon-home home-icon"></i>
+   <a href="<?php echo base_url();?>">Home</a>
+   <span class="divider">
+    <i class="icon-angle-right arrow-icon"></i>
+   </span>
+  </li>
+  <li class="active"><a href="<?php echo base_url('/index.php/skeleton/groups');?>"><?php echo lang('groups');?></a></li>
+ </ul>
+</div>
+
+ <div class="page-content">
+
+  <div class="page-header position-relative">
+                        <h1>
+                            <a href="<?php echo base_url('index.php/skeleton/groups');?>"><?php echo lang("groups");?></a>
+                        </h1>
+  </div>
+
+
+ <!--Grocery Crud -->
+ <?php echo $output; ?>
+ </div>
+</div>
---
+++ application/views/include/user_preferences_NOT_yet_header.php
@@ -0,0 +1,10 @@
+<div id="message-box" class="span12">
+ <div class="alert alert-info">
+  <a class="close" href="#" data-dismiss="alert"> x </a>
+   <?php echo lang('user_preferences_not_yet_message1');?><br/>
+   <?php echo lang('user_preferences_not_yet_message2');?>
+    <a class="go-to-edit-form" href="<?php echo base_url('index.php/skeleton/user_preferences/add')?>">
+	 <?php echo lang('here');?>
+	</a>
+ </div>
+</div>
