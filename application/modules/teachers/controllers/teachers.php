bb79a668374b0775e32f589f3cb01efc1da474f7
Oscar Adan <oadan79@gmail.com>
2014-05-12 12:41:06 +0200

    Generated Teachers sheet pdf

--- application/modules/teachers/controllers/teachers.php
+++ application/modules/teachers/controllers/teachers.php
@@ -24,6 +24,11 @@ class teachers extends skeleton_main {
         $this->load->add_package_path(APPPATH.'third_party/image-crud/application/');
     		$this->load->library('image_CRUD');  
 
+        // Load FPDF        
+        $this->load->add_package_path(APPPATH.'third_party/fpdf-codeigniter/application/');
+        $params = array ('orientation' => 'P', 'unit' => 'mm', 'size' => 'A4', 'font_path' => 'font/');        
+        $this->load->library('pdf',$params); // Load library
+
 		    /* Set language */
 		    $current_language=$this->session->userdata("current_language");
 		    if ($current_language == "") {
@@ -36,7 +41,204 @@ class teachers extends skeleton_main {
 	}
 
     public function teacher_sheet() {
-        echo "TODO";
+
+        if (!$this->skeleton_auth->logged_in())
+        {
+            //redirect them to the login page
+            redirect($this->skeleton_auth->login_page, 'refresh');
+        }
+
+        //TODO
+        $active_menu = array();
+        $active_menu['menu']='#reports';
+        $active_menu['submenu1']='#teachers_reports';
+        $active_menu['submenu2']='#teachers_sheet_report';
+        
+        $this->load->model('teachers_model');
+
+        $all_teachers = $this->teachers_model->get_all_teachers();
+
+        $default_group_code = $this->config->item('default_group_code');
+
+        $group_code=$default_group_code;
+
+        $organization = $this->config->item('organization','skeleton_auth');
+
+        $header_data['header_title']=lang("all_teachers") . ". " . $organization;
+
+        $contador = 0;
+        $professor = array();
+      
+        foreach($all_teachers as $teacher) {
+
+            $professor[$contador]['code']=$teacher->teacher_id;
+            $professor[$contador]['name']=$teacher->givenName;
+            $professor[$contador]['sn1']=$teacher->sn1;
+            $professor[$contador]['sn2']=$teacher->sn2;
+
+            if( file_exists(getcwd().'/uploads/person_photos/'.$teacher->photo_url)) {
+            
+                $professor[$contador]['photo']=base_url('uploads/person_photos')."/".$teacher->photo_url;
+            } else {
+                $professor[$contador]['photo']=base_url('assets/img/alumnes/foto.png');
+            }
+
+            $professor[$contador]['carrec']="Càrrec ".$professor[$contador]['code'];
+
+            $contador++;
+        }
+
+        $count = count($professor);
+     
+
+        //Crido la classe
+        $pdf = new FPDF('P', 'mm', 'A4','font/');
+        //Defineixo els marges
+        $pdf->SetMargins(10,10,10);
+        //Obro una pàgina
+        $pdf->AddPage();
+                //$pdf->Image($jpeg_file_cons[0],166,222,10);        
+        //$pdf->AddPage("P","A3");
+        //Es la posicio exacta on comença a escriure
+        $x=7;//10
+        $y=15;//24
+
+        //Logo
+        $pdf->Image(base_url().APPPATH.'third_party/skeleton/assets/img/logo_iesebre_2010_11.jpg',$x+2,5,40,15);
+        //Defineixo el tipus de lletra, si és negreta (B), si és cursiva (L), si és normal en blanc
+        $pdf->SetFont('Arial','B',15);
+        //$pdf->Cell(Amplada, altura, text, marc, on es comença a escriure després, alineació)
+        $pdf->SetXY(10,10);
+        $any_comencament = 2013;
+        $any_finalitzacio = 2014;
+        $pdf->Cell(190,6,"PROFESSORAT ".$any_comencament."-".$any_finalitzacio,0,0,'C');
+        $y=$y+6;
+
+        //Guardo les coordenades inicials de x i y
+        $x_start=$x;
+        $y_start=$y;
+
+        //Inicio les columnes i les files a 0
+        $col=0;
+        $row=0;
+
+        //Paràmetres de tamany de les fotos, $xx indica l'amplada de la foto, $yy indica
+        //l'altura de cada camp del professor, l'altura de la foto es 3 vegades aquest valor
+        //En cas de tocar aquest paràmetres caldria revisar el màxim de columnes i files  
+        $xx=11;//10//Amplada horitzontal de cada professor es tocada segons el nombre de professors que hi haguin
+
+        //Sergi Tur
+        //Si no s'indica l'amplada vertical es posa el que toca per mantenir les proporcions
+        //Fotos originals: 147x186:1.265306122
+        //Mida: 12x15,183673464
+        //$yy=5;//3//Amplada vertical de cada professor es tocada segons el nombre de professors que hi haguin
+
+        //No és l'açada de la FOTO! És la alçada del que ocupa cada bloc de profe (foto+dades)
+        $yy=4.75;
+
+        //Amb aquestes fòrmules defineixo les coordenades de cada camp de cada professor
+        //Fòrmula: posició inicial de x/y * columna * camps de cada professor 
+
+        //Ampla de la columna amb el nom i cognoms del professor
+        $x_name=12;
+        //Ampla de la columna de carrecs
+        $x_post=9;
+
+        $x=$x_start+$col*($xx+$x_name+$x_post);
+        $x1=$x_start+$col*($xx+$x_name+$x_post)+$x_name;
+        $x2=$x_start+$col*($xx+$x_name+$x_post)+$x_name+$x_post;
+
+        $y=$y_start+$row*$yy*3;
+        $y1=$y_start+$row*$yy*3+$yy;
+        $y2=$y_start+$row*$yy*3+$yy*2;
+        $y3=$y_start+$row*$yy*3+$yy*2;
+
+        //La i és el marge entre professors
+        $i=0;
+        $page_one=true;
+
+        //Imprimeixo sempre els conserges i secretàries en una posició fixa el primer cop
+        //TODO: Obtenir les dades de les carpetes personal de Gosa:
+                
+        //Posició inicial conserges:
+
+            $initial_x_personal=166;
+            $initial_y_personal=222;
+
+            $width_personal_foto=10;
+
+
+        {
+
+        $x = $x -22;
+        //$y = 21;
+        for($j=0;$j<$count; $j++) {
+
+                $pdf->SetFont('Arial','B',6);
+                $pdf->SetTextColor(255,0,0);
+                
+                $pdf->Text($x+22,$y,utf8_decode($professor[$j]['code']));
+                
+                $pdf->SetFont('Arial','',4);
+                $pdf->SetTextColor(0,0,0);      
+                $pdf->Text($x+44,$y,utf8_decode($professor[$j]['carrec']));
+                $pdf->Text($x+22,$y1-1,utf8_decode($professor[$j]['name']));
+                $pdf->Text($x+22,$y2-2,utf8_decode($professor[$j]['sn1']));
+                $pdf->Text($x+22,$y+11,utf8_decode($professor[$j]['sn2']));
+                //$pdf->Image($jpeg_file[$j],$x1-2,$y-2,$xx);                
+                $pdf->Image($professor[$j]['photo'],$x1-2,$y-2,$xx);                
+            //incremento la fila
+            $row++;
+            //incremento el marge
+            $i=$i+0.3;
+
+            //Recàlculo les coordenades
+            $y=$y_start+$i+$row*$yy*3;
+            $y1=$y_start+$i+$row*$yy*3+$yy;
+            $y2=$y_start+$i+$row*$yy*3+$yy*2;
+
+            //màxim de files per pàgina 
+            if($row>17){//26//Maxim de registre per columnes si es toca el tamny del professor tambe es tocara aquesta dada.
+                //incremento la columna
+                $col++;
+                //reinicio les files i el marge
+                $row=0;
+                $i=0;
+                //Recàlculo les coordenades
+                $x=$x_start+$col*($xx+$x_name+$x_post)-22;   
+                $x1=$x_start+$col*($xx+$x_name+$x_post)+$x_name;
+                $x2=$x_start+$col*($xx+$x_name+$x_post)+$x_name+$x_post;
+                
+                $y=$y_start+$i+$row*$yy*3;
+                $y1=$y_start+$i+$row*$yy*3+$yy;
+                $y2=$y_start+$i+$row*$yy*3+$yy*2;
+
+            }
+            //Quan arribem a la última fila vigilem de no escriure a sobre dels conserges i secretàries
+            if($col==5 && $row==21 && $page_one){
+                //Ho tornem a posar tot a 0 i obrim una nova pàgina
+                $col=0;
+                $row=0;
+                $i=0;
+                $x=$x_start+$col*$xx;
+                $x1=$x_start+$col*$xx*3+$xx;
+                $x2=$x_start+$col*$xx*3+$xx*2;
+
+                $y=$y_start+$i+$row*$yy*3;
+                $y1=$y_start+$i+$row*$yy*3+$yy;
+                $y2=$y_start+$i+$row*$yy*3+$yy*2;
+                $page_one=false;
+                $pdf->AddPage();
+            }
+        }
+        }
+
+        //enviem tot al pdf
+        $pdf->Output("Professorat_".$any_comencament."-".$any_finalitzacio."_(".date("d-m-Y").").pdf", "I");
+
+        
+//    }
+
     }
 
     function tutors_report() { // Tutors de Grup
@@ -261,5 +463,10 @@ function renderitzar($table_name,$header_data)
         return $header_data;
 
   }
+
+        function cmpTeachers($a, $b)    {
+            return strnatcmp($a->code, $b->code);
+        }
+
 
 }
