b15b9e3362d409ff7a7979980e7fb7a11d5c18f8
Sergi Tur <sergitur@ebretic.com>
2014-07-01 03:15:40 +0200

    Sync

---
+++ migration_scripts/migrate_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/bin/php5 /usr/share/ebre-escool/migration_scripts/localities_frompostalcode.php &> /usr/share/ebre-escool/migration/result_localities_frompostalcode.sql
+/usr/bin/php5 /usr/share/ebre-escool/migration_scripts/personsFromLdapToDatabase.php &> /usr/share/ebre-escool/migration/result_personsFromLdapToDatabase.txt
+/usr/bin/php5 /usr/share/ebre-escool/migration_scripts/enroll.php &> /usr/share/ebre-escool/migration/result_enroll.txt
+
+
+    
