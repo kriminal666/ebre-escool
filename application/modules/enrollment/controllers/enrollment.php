c3ea2906cf17b3d598b0e2129aad15157e24bf48
Sergi Tur Badenas <sergitur@ebretic.com>
2014-07-01 02:23:08 +0200

    Sync

--- application/modules/enrollment/controllers/enrollment.php
+++ application/modules/enrollment/controllers/enrollment.php
@@ -706,6 +706,112 @@ function load_ace_files($active_menu,$header_data=false){
         return $header_data;
 }
 
+
+
+public function upload_photo() {
+
+     //please note that the request will fail if you upload a file larger than what is supported by your PHP or Webserver settings
+
+     $ajax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH']==='XMLHttpRequest';
+
+     $result = array();
+
+     $file = $_FILES['avatar'];
+     if(!preg_match('/^image\//' , $file['type']) 
+        || !preg_match('/\.(jpe?g|gif|png)$/' , $file['name'])
+            || getimagesize($file['tmp_name']) === FALSE
+        ) {
+        $result['status'] = 'ERR';
+        $result['message'] = 'Invalid file format!';
+     }
+     else if($file['size'] > 110000) {
+        $result['status'] = 'ERR';
+        $result['message'] = 'Please choose a smaller file!';
+     }
+     else if($file['error'] != 0 || !is_uploaded_file($file['tmp_name'])) {
+        $result['status'] = 'ERR';
+        $result['message'] = 'Unspecified error!';
+     }
+     else {
+        
+        //$filename = $file['name'];
+        $extension = strtolower(preg_replace('/^.*\./', '', $file['name']));
+        $base_filename = $_POST['official_id'] . "_" . $_POST['username'];
+        $filename = $base_filename . "." . $extension;
+        $save_path = "/usr/share/ebre-escool/uploads/person_photos/test_" . $filename;
+        $thumb_path = "/usr/share/ebre-escool/uploads/person_photos/test_thumb_" . $filename;
+
+        if( 
+            ! move_uploaded_file($file['tmp_name'] , $save_path)
+            OR
+            !$this->resize($save_path, $thumb_path, 150) 
+          )
+        {
+            $result['status'] = 'ERR';
+            $result['message'] = 'Unable to save file!';
+        }
+
+        else {
+            $result['status'] = 'OK';
+            $result['message'] = 'Avatar changed successfully!';
+            $result['url'] = base_url('uploads/person_photos') . "/test_thumb_" . $filename;
+        }
+     }
+
+
+     $result = json_encode($result);
+     if($ajax) {
+        echo $result;
+     }
+     else {
+        //for browsers that don't support uploading via ajax,
+        //we have used an iframe instead and the response is sent as a script
+        echo '<script language="javascript" type="text/javascript">';
+        echo 'var iframe = window.top.window.jQuery("#'.$_POST['temporary-iframe-id'].'").data("deferrer").resolve('.$result.');';
+        echo '</script>';
+     }
+
+}
+
+protected function resize($in_file, $out_file, $new_width, $new_height=FALSE)
+{
+    $image = null;
+    $extension = strtolower(preg_replace('/^.*\./', '', $in_file));
+    switch($extension)
+    {
+        case 'jpg':
+        case 'jpeg':
+            $image = imagecreatefromjpeg($in_file);
+        break;
+        case 'png':
+            $image = imagecreatefrompng($in_file);
+        break;
+        case 'gif':
+            $image = imagecreatefromgif($in_file);
+        break;
+    }
+    if(!$image || !is_resource($image)) return false;
+
+
+    $width = imagesx($image);
+    $height = imagesy($image);
+    if($new_height === FALSE)
+    {
+        $new_height = (int)(($height * $new_width) / $width);
+    }
+
+    
+    $new_image = imagecreatetruecolor($new_width, $new_height);
+    imagecopyresampled($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+    $ret = imagejpeg($new_image, $out_file, 80);
+
+    imagedestroy($new_image);
+    imagedestroy($image);
+
+    return $ret;
+}
+
 public function get_last_study_id () {
 
     if(isset($_POST['student_id'])) {
--- application/modules/enrollment/views/wizard.php
+++ application/modules/enrollment/views/wizard.php
@@ -582,12 +582,20 @@ STEP 6 - ALL SUB-MODULES FROM SELECTED MODULES
             </div><!-- /step-container -->
             <hr />
             <div class="row-fluid wizard-actions">
+              <button id="print_enrollment" class="btn btn-purple">
+                Imprimir matrícula
+                <i class="icon-print icon-on-right"></i>
+              </button>
+              <button id="print_tic_enrollment" class="btn btn-info">
+                Imprimir matrícula TIC
+                <i class="icon-print icon-on-right"></i>
+              </button>
               <button class="btn btn-prev">
                 <i class="icon-arrow-left"></i>
-                Prev
+                Anterior
               </button>
-              <button class="btn btn-success btn-next" data-last="Finish ">
-                Next
+              <button class="btn btn-success btn-next" data-last="Finalitzar ">
+                Següent
                 <i class="icon-arrow-right icon-on-right"></i>
               </button>
             </div><!-- /wizard-actions -->
@@ -845,7 +853,129 @@ STEP 6 - ALL SUB-MODULES FROM SELECTED MODULES
               }
             },
               url: function(params) {
+
+                    //
+
+                    //please modify submit_url accordingly
+                    var submit_url = '<?php echo base_url("index.php/enrollment/upload_photo");?>';
+                    var deferred;
+
+
+                    //if value is empty, means no valid files were selected
+                    //but it may still be submitted by the plugin, because "" (empty string) is different from previous non-empty value whatever it was
+                    //so we return just here to prevent problems
+                    var value = $('#avatar').next().find('input[type=hidden]:eq(0)').val();
+                    if(!value || value.length == 0) {
+                      deferred = new $.Deferred
+                      deferred.resolve();
+                      return deferred.promise();
+                    }
+                    
+                    username = "sergitur";
+                    official_id = "14268002K";
+
+                    var $form = $('#avatar').next().find('.editableform:eq(0)');
+                    $form.append('<input type="hidden" name="username" value="'+username+'" />');
+                    $form.append('<input type="hidden" name="official_id" value="'+official_id+'" />');
+                    var file_input = $form.find('input[type=file]:eq(0)');
+
+                    //user iframe for older browsers that don't support file upload via FormData & Ajax
+                    if( !("FormData" in window) ) {
+                      deferred = new $.Deferred
+
+                      var iframe_id = 'temporary-iframe-'+(new Date()).getTime()+'-'+(parseInt(Math.random()*1000));
+                      $form.after('<iframe id="'+iframe_id+'" name="'+iframe_id+'" frameborder="0" width="0" height="0" src="about:blank" style="position:absolute;z-index:-1;"></iframe>');
+                      $form.append('<input type="hidden" name="temporary-iframe-id" value="'+iframe_id+'" />');
+                      
+
+                      $form.next().data('deferrer' , deferred);//save the deferred object to the iframe
+                      $form.attr({'method' : 'POST', 'enctype' : 'multipart/form-data',
+                            'target':iframe_id, 'action':submit_url});
+
+                      $form.get(0).submit();
+
+                      //if we don't receive the response after 60 seconds, declare it as failed!
+                      setTimeout(function(){
+                        var iframe = document.getElementById(iframe_id);
+                        if(iframe != null) {
+                          iframe.src = "about:blank";
+                          $(iframe).remove();
+                          
+                          deferred.reject({'status':'fail','message':'Timeout!'});
+                        }
+                      } , 60000);
+                    }
+                    else {
+                      var fd = null;
+                      try {
+                        fd = new FormData($form.get(0));
+                      } catch(e) {
+                        //IE10 throws "SCRIPT5: Access is denied" exception,
+                        //so we need to add the key/value pairs one by one
+                        fd = new FormData();
+                        $.each($form.serializeArray(), function(index, item) {
+                          fd.append(item.name, item.value);
+                        });
+                        //and then add files because files are not included in serializeArray()'s result
+                        $form.find('input[type=file]').each(function(){
+                          if(this.files.length > 0) fd.append(this.getAttribute('name'), this.files[0]);
+                        });
+                      }
+                      
+                      //if file has been drag&dropped , append it to FormData
+                      if(file_input.data('ace_input_method') == 'drop') {
+                        var files = file_input.data('ace_input_files');
+                        if(files && files.length > 0) {
+                          fd.append(file_input.attr('name'), files[0]);
+                        }
+                      }
+
+                      deferred = $.ajax({
+                        url: submit_url,
+                        type: 'POST',
+                        processData: false,
+                        contentType: false,
+                        dataType: 'json',
+                        data: fd,
+                        xhr: function() {
+                          var req = $.ajaxSettings.xhr();
+                          /*if (req && req.upload) {
+                            req.upload.addEventListener('progress', function(e) {
+                              if(e.lengthComputable) {  
+                                var done = e.loaded || e.position, total = e.total || e.totalSize;
+                                var percent = parseInt((done/total)*100) + '%';
+                                //bar.css('width', percent).parent().attr('data-percent', percent);
+                              }
+                            }, false);
+                          }*/
+                          return req;
+                        },
+                        beforeSend : function() {
+                          //bar.css('width', '0%').parent().attr('data-percent', '0%');
+                        },
+                        success : function() {
+                          //bar.css('width', '100%').parent().attr('data-percent', '100%');
+                        }
+                      })
+                    }
+
+
+
+                    deferred.done(function(res){
+                      if(res.status == 'OK') $('#avatar').get(0).src = res.url;
+                      else alert(res.message);
+                    }).fail(function(res){
+                      alert("Failure");
+                    });
+
+
+                    return deferred.promise();
+
+
+
+
               // ***UPDATE AVATAR HERE*** //
+              /*
               //You can replace the contents of this function with examples/profile-avatar-update.js for actual upload
       
               var deferred = new $.Deferred
@@ -880,6 +1010,9 @@ STEP 6 - ALL SUB-MODULES FROM SELECTED MODULES
                } , parseInt(Math.random() * 800 + 800))
       
               return deferred.promise();
+
+              */
+
             },
             
             success: function(response, newValue) {
@@ -890,6 +1023,10 @@ STEP 6 - ALL SUB-MODULES FROM SELECTED MODULES
       }
 
       jQuery(function($) {
+
+          $("#print_tic_enrollment").hide();
+          $("#print_enrollment").hide();
+          
 
           $('.date-picker').datepicker({"autoclose": true});
           $('.input-mask-date').mask('99-99-9999'); 
@@ -1227,6 +1364,43 @@ STEP 6 - ALL SUB-MODULES FROM SELECTED MODULES
         $('[name ^="' + step_to_green + '"]').addClass("green");
       });
 
+      
+
+      $("#print_tic_enrollment").click(function(){
+          console.debug("print_tic_enrollment click!");
+
+          student_generated_password = $("#step0 input[name$='person_generated_password']").val();
+          student_password = $("#step0 input[name$='person_password']").val();
+          student_verify_password = $("#step0 input[name$='person_verify_password']").val();
+
+          password = "";
+          if ( student_password == "") {
+              password = student_generated_password;            
+          } else if ( student_password != student_verify_password ) {
+            console.debug("ERROR. student_password and student_verify_password are not the same");
+            return false;
+          } else {
+              password = student_password;
+          }
+
+          person_id = $("#step0 input[name$='person_id']").val();
+
+          console.debug("password: " + password);
+          console.debug("person_id: " + person_id);
+
+          var iframe = $('<iframe height="750" width="1000">');
+          iframe.attr("src","<?php echo base_url('index.php/enrollment/enrollment_pdf?password=');?>" + password + "&person_id=" + person_id);
+          $('#enrollment_pdf_dialog').append(iframe);
+
+
+
+      });
+
+      $("#print_enrollment").click(function(){
+          console.debug("print_enrollment click!");
+      });
+      
+
 
       $(".btn-next").click(function(){
         console.debug("Click on next button");
@@ -1343,15 +1517,15 @@ STEP 6 - ALL SUB-MODULES FROM SELECTED MODULES
 
           academic_period = $("#academic_period option:selected").text();
           
-          student_name = $("#student option:selected").text();
+          student_name = $("#student option:selected").text().trim();
           student_id = $("#student option:selected").val();
           var res = student_name.split("."); 
           student_dni = res[0].trim();
           
-          console.debug("academic_period: " + academic_period);
-          console.debug("student_dni: " + student_dni);
-          console.debug("student_name: " + student_name);
-          console.debug("student_id: " + student_id);
+          //console.debug("academic_period: " + academic_period);
+          //console.debug("student_dni: " + student_dni);
+          //console.debug("student_name: " + student_name);
+          //console.debug("student_id: " + student_id);
 
           prepare_step2(student_id);
           
@@ -1790,6 +1964,9 @@ STEP 6 - ALL SUB-MODULES FROM SELECTED MODULES
         }
         $("[name = 'step6_title' ]").addClass("green"); 
 
+        $("#print_tic_enrollment").show();
+        $("#print_enrollment").show();
+
 // End step 5
 
 /***********
@@ -1849,70 +2026,18 @@ STEP 6 - ALL SUB-MODULES FROM SELECTED MODULES
           }
         }).on('finished', function(e) {
 
-          console.debug("FINISH!");
-
-          student_generated_password = $("#step0 input[name$='person_generated_password']").val();
-          student_password = $("#step0 input[name$='person_password']").val();
-          student_verify_password = $("#step0 input[name$='person_verify_password']").val();
-
-          password = "";
-          if ( student_password == "") {
-              password = student_generated_password;            
-          } else if ( student_password != student_verify_password ) {
-            console.debug("ERROR. student_password and student_verify_password are not the same");
-            return false;
-          } else {
-              password = student_password;
-          }
-
-          person_id = $("#step0 input[name$='person_id']").val();
-
-          console.debug("password: " + password);
-          console.debug("person_id: " + person_id);
-
-          var iframe = $('<iframe height="750" width="1000">');
-          iframe.attr("src","<?php echo base_url('index.php/enrollment/enrollment_pdf?password=');?>" + password + "&person_id=" + person_id);
-          $('#enrollment_pdf_dialog').append(iframe);
-
-          console.debug("Xivato2");
-
-          //Print enrollment PDF          
-          $("#enrollment_pdf_dialog").dialog({
-
-              resizable: true,
-              height:800,
-              width: 1050,
-              modal: true,
-              buttons: {
-                "Ok": function() {
-                    $( this ).dialog( "close" );
-
-                    bootbox.dialog({
-                      message: "La matrícula s'ha realitzat correctament!", 
-                      buttons: {
-                        "success" : {
-                          "label" : "OK",
-                          "className" : "btn-sm btn-primary",
-                          callback: function() {
-                                      window.location.href = "<?php echo base_url('index.php/enrollment/wizard');?>";
-                                }
-                        }
+          bootbox.dialog({
+            message: "La matrícula s'ha realitzat correctament!", 
+            buttons: {
+              "success" : {
+                "label" : "OK",
+                "className" : "btn-sm btn-primary",
+                callback: function() {
+                            window.location.href = "<?php echo base_url('index.php/enrollment/wizard');?>";
                       }
-                    });
-
-                  },
-                Cancel: function() {
-                    $( this ).dialog( "close" );
-                  }
               }
-
-          }); 
-
-          
-
-          //NEXT enrollment
-          //window.location.href = '<?php echo base_url('index.php/enrollment/wizard');?>';
-
+            }
+          });          
 
         }).on('stepclick', function(e){
           //return false;//prevent clicking on steps
