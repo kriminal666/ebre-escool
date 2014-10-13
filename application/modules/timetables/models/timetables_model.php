bb00d13e7016ee853bd1f32ea00b435e10872d9a
Sergi Tur Badenas <sergitur@ebretic.com>
2014-10-13 11:26:28 +0200

    Sync

--- application/modules/timetables/models/timetables_model.php
+++ application/modules/timetables/models/timetables_model.php
@@ -184,7 +184,7 @@ JOIN classroom_group ON classroom_group.classroom_group_id = lesson.lesson_class
 		$this->db->where('lesson_academic_period_id',$current_academic_period_id);
         
         $query = $this->db->get();	
-     	echo $this->db->last_query();
+     	//echo $this->db->last_query();
 
         if ($query->num_rows() > 0) {
 
