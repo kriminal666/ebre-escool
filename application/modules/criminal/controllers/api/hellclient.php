ef8430a52c1491345759183f4c26d11a5fb1041d
kriminal666 <piltrafiya_666@hotmail.com>
2014-12-19 22:02:33 +0100

    Cambios para hacer commit: 	deleted:    application/modules/criminal/config/autoload.php 	modificado: application/modules/criminal/controllers/api/hellclient.php 	modificado: application/modules/criminal/libraries/Rest.php
    orain restclient ondo dijoa ..

--- application/modules/criminal/controllers/api/hellclient.php
+++ application/modules/criminal/controllers/api/hellclient.php
@@ -1,43 +1,45 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-include "application/third_party/skeleton/application/controllers/skeleton_main.php";
 
-
-class HellClient extends skeleton_main {
-
-	//URL's
-	public $getTeacher_Url = 'criminal/api/hell/teacher/';
-	public $getAllTeacher_Url = 'criminal/api/hell/teachers/';
+class HellClient extends CI_Controller {
 
 
 	function __construct()
     {
-        parent::__construct();
-        $this->load->library('rest', array(
-           'server' => 'http://localhost/ebre-escool/index.php/'));
+       parent::__construct();
+        $this->load->library('Rest');
+        $config = array('server' =>'http://localhost/ebre-escool/index.php/');
+        $this->rest->initialize($config);
+    
         //We can add:
         // 'http_user' => 'admin',
         // 'http_pass' => '1234',
         // 'http_auth' => 'basic' // or 'digest'
         // Load the rest client spark
-        $this->load->library('curl');
+        $this->load->library('Curl');
     }
 
     
 
      //Get one or all teacher 
      function getTeacher($id = null){
+        
+       
+       $getTeacher_Url = 'criminal/api/hell/teacher/id/';
+       $getAllTeacher_Url = 'criminal/api/hell/teachers/';
+
      	//If not null get just one teacher by 'id'
-     	if ($id != null){
+     	     if($id!=null){
             // We call the function teacher_get of hell controller
-     		$this->rest->get('criminal/api/hell/teacher',array('id' => $id));
+     		$teacher = $this->rest->get($getTeacher_Url,array('id'=>$id));
 
-     	}else{
+        }else{
+     
      		//we want to get all teachers from database
-     		$this->rest->get($getAllTeacher_Url,null);
-     	}
-
-     }
+     		$teacher = $this->rest->get($getAllTeacher_Url,null);
+        }
+        echo json_encode($teacher);
+    }
  }
 
  ?>
