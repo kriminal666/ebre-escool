497513ba94d53ab884ded7e92a2c677d1112e4c9
Sergi Tur <sergiturbadenas@gmail.com>
2014-12-19 17:05:21 +0100

    First version. Skeleton of REST API

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -382,8 +382,9 @@ abstract class REST_Controller extends CI_Controller
         }
 
         // Sure it exists, but can they do anything with it?
+        
         if ( ! method_exists($this, $controller_method)) {
-            $this->response(array(config_item('rest_status_field_name') => false, config_item('rest_message_field_name') => 'Unknown method.'), 404);
+            $this->response(array(config_item('rest_status_field_name') => false, config_item('rest_message_field_name') => 'Unknown method ' . $controller_method . '.'), 404);
         }
 
         // Doing key related stuff? Can only do it if they have a key right?
