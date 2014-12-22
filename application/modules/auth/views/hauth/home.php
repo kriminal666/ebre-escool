ed5a08d260041eebc2d9cad303cfaf9adc9a65d0
Sergi Tur <sergiturbadenas@gmail.com>
2014-12-22 12:16:31 +0100

    Sync

---
+++ application/modules/auth/views/hauth/home.php
@@ -0,0 +1,10 @@
+<?php
+	$this->load->helper('url');
+	echo anchor('auth/hauth/login/Google','Login With Google.').'<br />';
+	
+	echo anchor('auth/hauth/login/Twitter','Login With Twitter.').'<br />';
+	
+	echo anchor('auth/hauth/login/Facebook','Login With Facebook.').'<br />';
+	
+	echo anchor('auth/hauth/login/LinkedIn','Login With LinkedIn.').'<br />';
+?>
